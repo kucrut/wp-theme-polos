7d191b7576deb8d3ce27316a1f9cef88cd1a0190
Dzikri Aziz <kucrut@kucrut.org>
2011-12-26 18:40:03 +0800

    Cleanup menu

--- header.php
+++ header.php
@@ -35,5 +35,12 @@ $lang_attr = ob_get_clean();
 				<h2 id="site-description"><?php echo $site_desc ?></h2>
 				<?php } ?>
 			</hgroup>
-			<?php wp_nav_menu( array( 'theme_location' => 'main', 'container' => 'nav', 'container_id' => 'main-menu', 'container_class' => 'menu-container', 'fallback_cb' => false ) ); ?>
+			<?php wp_nav_menu( array(
+				'theme_location'	=> 'main',
+				'container'				=> 'nav',
+				'container_id'		=> 'main-menu',
+				'container_class'	=> 'menu-container main-menu-container',
+				'menu_class'			=> 'menu main-menu',
+				'fallback_cb'			=> false
+			) ); ?>
 		</header>
