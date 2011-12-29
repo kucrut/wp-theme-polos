7cdf02deec9740651996dadd98172d76036d50ca
Dzikri Aziz <kucrut@kucrut.org>
2011-12-29 20:49:22 +0800

    Fixed comments template

--- comments.php
+++ comments.php
@@ -7,20 +7,19 @@
 
 ?>
 
-	<?php if ( post_password_required() ) { ?>
 	<div id="comments">
+	<?php if ( post_password_required() ) { ?>
 		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'polos' ); ?></p>
 	</div>
 	<?php return; } ?>
 
 	<?php if ( have_comments() ) { ?>
-	<div id="comments">
 		<?php kct_response_list( get_the_ID() ); ?>
 
 		<?php if ( !comments_open() && !is_page() && post_type_supports(get_post_type(), 'comments') && get_comments_number() ) { ?>
 			<p class="nocomments"><?php _e( 'Comments are closed.', 'polos' ); ?></p>
 		<?php } ?>
 
-		<?php comment_form(); ?>
-	</div>
 	<?php } ?>
+	<?php comment_form(); ?>
+	</div>
