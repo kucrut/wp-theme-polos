26112b58192f9929361a07da715a3adabedb0ecd
Dzikri Aziz <kucrut@kucrut.org>
2011-12-29 09:19:09 +0800

    Improved comments template (no more empty element)

--- comments.php
+++ comments.php
@@ -4,21 +4,21 @@
  * @version 0.1
  */
 ?>
-	<div id="comments">
+
 	<?php if ( post_password_required() ) { ?>
+	<div id="comments">
 		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'baca' ); ?></p>
-	</div><!-- #comments -->
+	</div>
 	<?php return; } ?>
 
-	<?php
-		if ( have_comments() ) {
-			kct_response_list( get_the_ID() );
-		}
+	<?php if ( have_comments() ) { ?>
+	<div id="comments">
+		<?php kct_response_list( get_the_ID() ); ?>
 
-		if ( !comments_open() && !is_page() && post_type_supports(get_post_type(), 'comments') && get_comments_number() ) { ?>
-		<p class="nocomments"><?php _e( 'Comments are closed.', 'baca' ); ?></p>
+		<?php if ( !comments_open() && !is_page() && post_type_supports(get_post_type(), 'comments') && get_comments_number() ) { ?>
+			<p class="nocomments"><?php _e( 'Comments are closed.', 'baca' ); ?></p>
+		<?php } ?>
+
+		<?php comment_form(); ?>
+	</div>
 	<?php } ?>
-
-	<?php comment_form(); ?>
-
-</div><!-- #comments -->
