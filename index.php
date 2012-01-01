479e38a134cbf81f800e8fd78f0f14d2cf475249
Dzikri Aziz <kucrut@kucrut.org>
2012-01-01 22:53:34 +0800

    Added post type support for sub-template in main loop

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 			if ( have_posts() ) {
 				while ( have_posts() ) {
 					the_post();
-					get_template_part( 'content' );
+					get_template_part( 'content', get_post_type() );
 				}
 
 				kct_paginate_links();
