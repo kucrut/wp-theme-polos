1b659c4e268f616e1b2e1325420840166030031b
Dzikri Aziz <kucrut@kucrut.org>
2012-01-03 16:11:03 +0800

    Footer texts filter hooks

--- footer.php
+++ footer.php
@@ -11,9 +11,14 @@
 			<?php do_action( 'kct_before_sidebar_wa-bottom' ); ?>
 			<?php dynamic_sidebar( 'wa-bottom' ); ?>
 			<?php do_action( 'kct_after_sidebar_wa-bottom' ); ?>
-		<?php } else { ?>
-			<p class="nw cl">CopyLeft &copy; <?php echo date('Y') . ' ' . get_bloginfo( 'name', 'display' ) ?></p>
-			<p class="nw wp">Proudly powered by <a href="http://wordpress.org">WordPress</a><sup>&reg;</sup></p>
+		<?php
+			}
+			else {
+			$t_copy = sprintf( __('CopyLeft &copy; %d %s', 'polos'), date('Y'), get_bloginfo('name', 'display') );
+			$t_credits = sprintf( __('Proudly powered by %s', 'polos'), '<a href="http://wordpress.org">WordPress</a><sup>&reg;</sup>');
+		?>
+			<p class="nw cl"><?php echo apply_filters( 'kct_text_copyright', $t_copy ) ?></p>
+			<p class="nw wp"><?php echo apply_filters( 'kct_text_poweredby', $t_credits ) ?></p>
 		<?php } ?>
 		</footer>
 	</div>
