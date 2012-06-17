bf965efc4afaad8a82b7e950cbc3f5f4d69657c2
Dzikri Aziz <kucrut@kucrut.org>
2012-06-17 08:26:08 +0700

    Fix default background color

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@ if ( !function_exists('polos_setup') ) {
 		# Features
 		add_theme_support( 'custom-background', array(
 			'default-image'          => get_template_directory_uri() . '/i/bg-dark.png',
-			'default-color'          => '#151515',
+			'default-color'          => '151515',
 			'wp-head-callback'       => '',
 			'admin-head-callback'    => '',
 			'admin-preview-callback' => ''
