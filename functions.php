b159b5955554cb51fd340fcd30dcd871dfc36715
Dzikri Aziz <kucrut@kucrut.org>
2012-06-18 17:45:56 +0700

    Fix background color support

--- functions.php
+++ functions.php
@@ -25,11 +25,8 @@ if ( !function_exists('polos_setup') ) {
 
 		# Features
 		add_theme_support( 'custom-background', array(
-			'default-image'          => get_template_directory_uri() . '/i/bg-dark.png',
-			'default-color'          => '151515',
-			'wp-head-callback'       => '',
-			'admin-head-callback'    => '',
-			'admin-preview-callback' => ''
+			'default-image' => get_template_directory_uri() . '/i/bg-dark.png',
+			'default-color' => '151515'
 		) );
 		add_theme_support( 'automatic-feed-links' );
 
